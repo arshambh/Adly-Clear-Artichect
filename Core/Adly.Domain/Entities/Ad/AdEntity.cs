eaeea02066ec51430c322a97bbcc9db8b7838fd8
ArshamCoder <arshambh7@gmail.com>
2025-07-18 19:15:46 +0330

    .

--- Core/Adly.Domain/Entities/Ad/AdEntity.cs
+++ Core/Adly.Domain/Entities/Ad/AdEntity.cs
@@ -2,78 +2,143 @@
 using Adly.Domain.Common.ValueObjects;
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
+using Ardalis.GuardClauses;
 
 namespace Adly.Domain.Entities.Ad;
 
-public class AdEntity: BaseEntity<Guid>
+public class AdEntity : BaseEntity<Guid>
 {
     private readonly List<ImageValueObjects> _images = new();
+    private readonly List<LogValueObject> _changeLog = new();
 
-    public string? Name { get; private set; }
+    public string? Title { get; private set; }
     public string? Description { get; private set; }
 
     public Guid UserId { get; private set; }
 
     public IReadOnlyList<ImageValueObjects> Images => _images.AsReadOnly();
+    public IReadOnlyList<LogValueObject> ChangeLogs => _changeLog.AsReadOnly();
 
     public Guid CategoryId { get; private set; }
+    public Guid LocationId { get; private set; }
+
+    public AdState CurrentState { get; private set; }
+
+    public enum AdState
+    {
+        Pending,
+        Rejected,
+        Approved,
+        Deleted,
+        Expired,
+    }
+
+
+    public DomainResult ChangeState(AdState state, string? additionalMessage = null)
+    {
+        if (CurrentState == AdState.Approved && state is AdState.Rejected or AdState.Pending)
+            return new DomainResult(false, "This ad is already approved!");
+
+        CurrentState = state;
+        _changeLog.Add(new LogValueObject(DateTime.Now, "Ad State Changed", additionalMessage));
+        return DomainResult.None;
+
+    }
+
+
 
     private AdEntity()
     {
-        
+
     }
 
-    public static AdEntity Create(string? name, string? description, Guid? userId, Guid? categoryId)
+
+
+
+    public static AdEntity Create(string? title, string? description, Guid? userId, Guid? categoryId, Guid? locationId)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(title);
         ArgumentException.ThrowIfNullOrWhiteSpace(description);
 
-        if (userId == Guid.Empty || userId == null)
-            throw new InvalidOperationException("User Id Must Have A Value...");
+        // if (userId == Guid.Empty || userId == null)
+        //     throw new InvalidOperationException("User Id Must Have A Value...");
+        //
+        // if (categoryId == Guid.Empty || categoryId == null)
+        //     throw new InvalidOperationException("Category Id Must Have A Value...");
 
-        if (categoryId == Guid.Empty || categoryId == null)
-            throw new InvalidOperationException("Category Id Must Have A Value...");
+        Guard.Against.NullOrEmpty(userId, "Invalid User ID");
+        Guard.Against.NullOrEmpty(categoryId, "Invalid Category ID");
+        Guard.Against.NullOrEmpty(locationId, "Invalid Location ID");
 
 
-
-
-        return new AdEntity
+        var ad = new AdEntity
         {
             Id = Guid.NewGuid(),
-            Name = name,
+            Title = title,
             Description = description,
             UserId = userId.Value,
-            CategoryId = categoryId.Value
+            CategoryId = categoryId.Value,
+            CurrentState = AdState.Pending,
+            LocationId = locationId.Value
         };
+
+
+        ad._changeLog.Add(new LogValueObject(DateTime.Now, "Ad Created"));
+
+        return ad;
     }
 
 
-    public static AdEntity Create(Guid? id,string? name, string? description, Guid? userId, Guid? categoryId)
+    public static AdEntity Create(Guid? id, string? title, string? description, Guid? userId, Guid? categoryId, Guid? locationId)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(title);
         ArgumentException.ThrowIfNullOrWhiteSpace(description);
 
-        if (userId == Guid.Empty || userId == null)
-            throw new InvalidOperationException("User Id Must Have A Value...");
+        // if (userId == Guid.Empty || userId == null)
+        //     throw new InvalidOperationException("User Id Must Have A Value...");
+        //
+        // if (categoryId == Guid.Empty || categoryId == null)
+        //     throw new InvalidOperationException("Category Id Must Have A Value...");
+        //
+        // if (id == Guid.Empty || id == null)
+        //     throw new InvalidOperationException("Id Must Have A Value...");
 
-        if (categoryId == Guid.Empty || categoryId == null)
-            throw new InvalidOperationException("Category Id Must Have A Value...");
 
-        if (id == Guid.Empty || id == null)
-            throw new InvalidOperationException("Id Must Have A Value...");
+        Guard.Against.NullOrEmpty(id, "Invalid ID");
+        Guard.Against.NullOrEmpty(userId, "Invalid User ID");
+        Guard.Against.NullOrEmpty(categoryId, "Invalid Category ID");
+        Guard.Against.NullOrEmpty(locationId, "Invalid Location ID");
 
-
-        return new AdEntity
+        var ad = new AdEntity
         {
             Id = id.Value,
-            Name = name,
+            Title = title,
             Description = description,
             UserId = userId.Value,
-            CategoryId = categoryId.Value
+            CategoryId = categoryId.Value,
+            CurrentState = AdState.Pending,
+            LocationId = locationId.Value
         };
+        ad._changeLog.Add(new LogValueObject(DateTime.Now, "Ad Created"));
+        return ad;
     }
 
 
+    public void Edit(string title, string description, Guid? categoryId, Guid? locationId)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(title);
+        ArgumentException.ThrowIfNullOrWhiteSpace(description);
+
+        Guard.Against.NullOrEmpty(categoryId, "Invalid Category ID");
+        Guard.Against.NullOrEmpty(locationId, "Invalid Location ID");
 
 
+        Title = title;
+        Description = description;
+        CategoryId = categoryId.Value;
+        LocationId = locationId.Value;
+
+        _changeLog.Add(new LogValueObject(DateTime.Now, "Ad Edited"));
+        CurrentState = AdState.Pending;
+    }
 }
--- Tests/Adly.Domain.Tests/AdTests/AdEntityTests.cs
+++ Tests/Adly.Domain.Tests/AdTests/AdEntityTests.cs
@@ -13,14 +13,14 @@ public class AdEntityTests
         var name = "Test Ad";
         Guid? userId = null;
         Guid? categoryId = Guid.NewGuid();
+        Guid? locationId = Guid.NewGuid();
 
         // Act 
-        Action act = () => AdEntity.Create(name, description, userId, categoryId);
+        Action act = () => AdEntity.Create(name, description, userId, categoryId, locationId);
 
         // Assert
         act.Should()
-           .Throw<InvalidOperationException>()
-           .WithMessage("User Id Must Have A Value...");
+           .Throw<ArgumentException>();
 
     }
 
@@ -32,14 +32,14 @@ public class AdEntityTests
         var name = "Test Ad";
         Guid? userId = Guid.NewGuid();
         Guid? categoryId = null;
+        Guid? locationId = Guid.NewGuid();
 
         // Act 
-        Action act = () => AdEntity.Create(name, description, userId, categoryId);
+        Action act = () => AdEntity.Create(name, description, userId, categoryId, locationId);
 
         // Assert
         act.Should()
-            .Throw<InvalidOperationException>()
-            .WithMessage("Category Id Must Have A Value...");
+            .Throw<ArgumentException>();
 
     }
 
@@ -51,14 +51,14 @@ public class AdEntityTests
         var name = "Test Ad";
         Guid? userId = Guid.NewGuid();
         Guid? categoryId = Guid.Empty;
+        Guid? locationId = Guid.NewGuid();
 
         // Act 
-        Action act = () => AdEntity.Create(name, description, userId, categoryId);
+        Action act = () => AdEntity.Create(name, description, userId, categoryId, locationId);
 
         // Assert
         act.Should()
-            .Throw<InvalidOperationException>()
-            .WithMessage("Category Id Must Have A Value...");
+            .Throw<ArgumentException>();
 
     }
 
@@ -71,14 +71,14 @@ public class AdEntityTests
         var name = "Test Ad";
         Guid? userId = Guid.Empty;
         Guid? categoryId = Guid.NewGuid();
+        Guid? locationId = Guid.NewGuid();
 
         // Act 
-        Action act = () => AdEntity.Create(name, description, userId, categoryId);
+        Action act = () => AdEntity.Create(name, description, userId, categoryId, locationId);
 
         // Assert
         act.Should()
-            .Throw<InvalidOperationException>()
-            .WithMessage("User Id Must Have A Value...");
+            .Throw<ArgumentException>();
 
     }
 
@@ -92,13 +92,124 @@ public class AdEntityTests
         Guid? userId = Guid.NewGuid();
         Guid? categoryId = Guid.NewGuid();
         Guid? id = Guid.NewGuid();
-
+        Guid? locationId = Guid.NewGuid();
 
         // Act 
-        var ad1 = AdEntity.Create(id, name, description, userId, categoryId);
-        var ad2 = AdEntity.Create(id, name, description, userId, categoryId);
+        var ad1 = AdEntity.Create(id, name, description, userId, categoryId, locationId);
+        var ad2 = AdEntity.Create(id, name, description, userId, categoryId, locationId);
 
         ad1.Equals(ad2).Should().BeTrue();
     }
+
+
+    [Fact]
+    public void Creating_An_Ads_Should_Have_ChangeLog()
+    {
+        // Arrange
+        var description = "Test Description";
+        var name = "Test Ad";
+        Guid? userId = Guid.NewGuid();
+        Guid? categoryId = Guid.NewGuid();
+        Guid? locationId = Guid.NewGuid();
+
+        // Act 
+        var ad = AdEntity.Create(name, description, userId, categoryId, locationId);
+
+        // Assert
+        ad.ChangeLogs.Should().HaveCount(1);
+
+    }
+
+
+    [Fact]
+    public void Creating_An_Ads_State_From_Approved_To_Other_States_Is_Not_Allowed()
+    {
+        // Arrange
+        var description = "Test Description";
+        var name = "Test Ad";
+        Guid? userId = Guid.NewGuid();
+        Guid? categoryId = Guid.NewGuid();
+        Guid? locationId = Guid.NewGuid();
+
+        // Act 
+        var ad = AdEntity.Create(name, description, userId, categoryId, locationId);
+
+        // Assert
+        ad.ChangeState(AdEntity.AdState.Approved);
+
+        var result = ad.ChangeState(AdEntity.AdState.Rejected);
+
+        result.IsSuccess.Should().BeFalse();
+
+    }
+
+
+    [Fact]
+    public void Changing_Ad_State_Should_Log()
+    {
+        // Arrange
+        var description = "Test Description";
+        var name = "Test Ad";
+        Guid? userId = Guid.NewGuid();
+        Guid? categoryId = Guid.NewGuid();
+        Guid? locationId = Guid.NewGuid();
+
+        // Act 
+        var ad = AdEntity.Create(name, description, userId, categoryId, locationId);
+
+        var changeLogCount = ad.ChangeLogs.Count;
+
+        // Assert
+        ad.ChangeState(AdEntity.AdState.Approved);
+
+        var changeLogCountAfterChangeState = ad.ChangeLogs.Count;
+
+        changeLogCountAfterChangeState.Should().BeGreaterThan(changeLogCount);
+
+    }
+
+
+    [Fact]
+    public void Ad_State_Should_Be_Pending_After_Edit()
+    {
+        // Arrange
+        var description = "Test Description";
+        var name = "Test Ad";
+        Guid? userId = Guid.NewGuid();
+        Guid? categoryId = Guid.NewGuid();
+        Guid? locationId = Guid.NewGuid();
+
+        // Act 
+        var ad = AdEntity.Create(name, description, userId, categoryId, locationId);
+        ad.ChangeState(AdEntity.AdState.Approved);
+
+        ad.Edit("New Title", "New Description", categoryId, locationId);
+
+        ad.CurrentState.Should().Be(AdEntity.AdState.Pending);
+    }
+
+
+    [Fact]
+    public void Should_Create_Log_After_Editing_An_Ad()
+    {
+        // Arrange
+        var description = "Test Description";
+        var name = "Test Ad";
+        Guid? userId = Guid.NewGuid();
+        Guid? categoryId = Guid.NewGuid();
+        Guid? locationId = Guid.NewGuid();
+
+        // Act 
+        var ad = AdEntity.Create(name, description, userId, categoryId, locationId);
+        ad.ChangeState(AdEntity.AdState.Approved);
+
+        var currentChangeLogCount = ad.ChangeLogs.Count;
+
+        ad.Edit("New Title", "New Description", categoryId, locationId);
+
+        ad.ChangeLogs.Count.Should().BeGreaterThan(currentChangeLogCount);
+
+    }
+
 
 }
