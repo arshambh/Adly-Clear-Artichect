80d395d0746ffe8185f4f354a655b8bc23a97305
ArshamCoder <arshambh7@gmail.com>
2025-08-12 14:59:12 +0330

    Part 19- Implementing Persistence Infrastructure (3) - Configuring DbContext

---
+++ Infrastructure/Adly.Infrastructure.Persistence/Configurations/AdlyDbContext.cs
@@ -0,0 +1,60 @@
+﻿using Adly.Domain.Common;
+using Adly.Infrastructure.Persistence.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adly.Infrastructure.Persistence.Configurations;
+
+public class AdlyDbContext(DbContextOptions<AdlyDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.RegisterEntities<IEntity>(typeof(IEntity).Assembly);
+        modelBuilder.ApplyRestrictDeleteBehaviour();
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AdlyDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override int SaveChanges()
+    {
+        ApplyEntityChangeDates();
+        return base.SaveChanges();
+    }
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+        ApplyEntityChangeDates();
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        ApplyEntityChangeDates();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+    {
+        ApplyEntityChangeDates();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    private void ApplyEntityChangeDates()
+    {
+        var entities = base.ChangeTracker.Entries()
+            .Where(x => x is { Entity: IEntity, State: EntityState.Added } or { Entity: IEntity, State: EntityState.Modified });
+       
+        foreach (var entity in entities)
+        {
+            if (entity.State == EntityState.Added)
+                ((IEntity)entity.Entity).CreateDate = DateTime.Now;
+
+            if (entity.State == EntityState.Modified)
+                ((IEntity)entity.Entity).ModifiedDate = DateTime.Now;
+
+        }
+    }
+
+
+}
---
+++ Infrastructure/Adly.Infrastructure.Persistence/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adly.Infrastructure.Persistence.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void RegisterEntities<TEntityType>(this ModelBuilder builder, params Assembly[] assemblies)
+    {
+        var entityTypes = assemblies.SelectMany(x => x.ExportedTypes)
+            .Where(x => x is { IsClass: true, IsAbstract: false, IsPublic: true } &&
+                        typeof(TEntityType).IsAssignableFrom(x));
+
+        foreach (var entityType in entityTypes)
+        {
+            builder.Entity(entityType);
+        }
+
+    }
+
+    public static void ApplyRestrictDeleteBehaviour(this ModelBuilder modelBuilder)
+    {
+        var cascadeForeignKeys = modelBuilder.Model.GetEntityTypes()
+            .SelectMany(x => x.GetForeignKeys())
+            .Where(x => x is { IsOwnership: false, DeleteBehavior: DeleteBehavior.Cascade });
+
+        foreach (var cascadeForeignKey in cascadeForeignKeys)
+        {
+            cascadeForeignKey.DeleteBehavior = DeleteBehavior.Restrict;
+        }
+
+
+    }
+
+
+
+}
