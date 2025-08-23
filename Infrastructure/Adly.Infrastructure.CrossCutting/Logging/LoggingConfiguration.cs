1c5dadf586a9917294aabee3d56edcc8185cb0e9
ArshamCoder <arshambh7@gmail.com>
2025-08-23 08:01:58 +0330

    Part 29 - Clean Architecture And Logging - Cross Cutting Concerns

---
+++ Infrastructure/Adly.Infrastructure.CrossCutting/Logging/LoggingConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Exceptions;
+
+namespace Adly.Infrastructure.CrossCutting.Logging;
+
+public class LoggingConfiguration
+{
+    public static Action<HostBuilderContext, LoggerConfiguration> ConfigureLogger => (context, configuration) =>
+    {
+        var applicationName = context.HostingEnvironment.ApplicationName;
+        configuration
+            .Enrich.WithSpan()
+            .Enrich.WithEnvironmentName()
+            .Enrich.WithMachineName()
+            .Enrich.WithExceptionDetails().
+            Enrich.WithProperty("ApplicationName", applicationName);
+
+
+        if (context.HostingEnvironment.IsDevelopment())
+        {
+            configuration.WriteTo.Console().MinimumLevel.Information();
+            return;
+        }
+
+
+        if (context.HostingEnvironment.IsProduction())
+        {
+            configuration.WriteTo.Console().MinimumLevel.Error();
+            return;
+        }
+
+
+
+
+
+    };
+}
